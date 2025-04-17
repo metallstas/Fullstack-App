2e0552a1a5bea9ba5b6af07289112ded160d3ecc
Stas Khmylko <metallstas@mail.ru>
2025-04-17 10:43:48 +0300

    fix route

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 
 app.get('/tags', PostController.getLastTags)
 app.get('/posts', PostController.getAll)
-app.get('/posts/tags', PostController.getLastTags)
 app.get('/posts/:id', PostController.getOne)
 app.post(
     '/posts',
